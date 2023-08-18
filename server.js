445127f9dcf22736adffb087908dca524615ccc4
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-08-18 19:06:07 +0700

    commit

--- server.js
+++ server.js
@@ -5,13 +5,6 @@ const dotenv=require('dotenv')
 
 dotenv.config({path:'config/config.env'})
 
-mongoose.connect('mongodb://127.0.0.1:27017/ecommerce-poly', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-
-
-
 app.listen(process.env.PORT,async ()=>{
    await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce-poly', {
         useNewUrlParser: true,
