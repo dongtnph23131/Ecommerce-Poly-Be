f9d85be2e872e8568b7c30ab51f08b1c5212701a
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-08-18 16:31:00 +0700

    commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose=require('mongoose')
 const dotenv=require('dotenv')
 
 
-dotenv.config({path:'Server/config/config.env'})
+dotenv.config({path:'config/config.env'})
 
 mongoose.connect('mongodb://127.0.0.1:27017/ecommerce-poly')
 
