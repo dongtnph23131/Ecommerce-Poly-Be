1688f5391fda7f3c3bda245d2403f0a4279ad8ad
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-08-20 12:09:33 +0700

    a

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,7 +12,7 @@ exports.signup = async (req, res) => {
         }
         const hashPassword = await bcrypt.hash(password, 10)
         const user = await User.create({
-            name, email, password: hashPassword
+            name, email, password: hashPassword,avatar
         })
         user.password = undefined
 
