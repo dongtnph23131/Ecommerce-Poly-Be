920f8db1c5005fe08b4988b264cac355d90b9e80
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-09-07 19:13:30 +0700

    commit

--- routers/product.js
+++ routers/product.js
@@ -1,12 +1,13 @@
 const express=require('express')
 const { addProduct, getProducts, removeProduct, updateProduct, getProduct, getProductsAdmin} = require('../controllers/product')
-
+const {authenticate}=require("../middlewares/authenticate")
+const {authorization}=require("../middlewares/authorization")
 const router=express.Router()
 
-router.post('/products',addProduct)
+router.post('/products',authenticate,authorization,addProduct)
 router.get('/products',getProducts)
-router.delete('/products/:id',removeProduct)
-router.patch('/products/:id',updateProduct)
+router.delete('/products/:id',authenticate,authorization,removeProduct)
+router.patch('/products/:id',authenticate,authorization,updateProduct)
 router.get('/products/:id',getProduct)
 router.get('/admin/products',getProductsAdmin)
 module.exports=router
