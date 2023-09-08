fe25a3a797c9096992d49049d031a605096b02e3
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-09-08 22:22:31 +0700

    commit

--- models/order.js
+++ models/order.js
@@ -33,7 +33,7 @@ const orderSchema = new mongoose.Schema({
                 type: String,
                 required: true,
             },
-            product: {
+            productId: {
                 type: mongoose.Schema.ObjectId,
                 ref: "Product",
                 required: true,
