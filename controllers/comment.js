8fe0dd43e259037bf9dd9e3ad6c0b61b46e7a258
dongtnph21313 <dongtnph21313@fpt.edu.vn>
2023-09-13 21:18:02 +0700

    commit

--- controllers/comment.js
+++ controllers/comment.js
@@ -16,6 +16,7 @@ exports.createCommentByProduct = async (req, res) => {
             data: product
         })
     }
+    //
     catch (error) {
         return res.status(400).json({
             message: error.message
